9064f0b907a0edce2dfc115a5c3a0f650691a4de
stepanowon <stepanowon@hotmail.com>
2025-01-03 14:04:48 +0900

    docker 설치 구문 변경경

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,20 +2,19 @@
 
 set -euo pipefail
 
-apt update -y
-apt-get install apt-transport-https ca-certificates curl -y
+apt-get update
+apt-get install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
-
-add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) \
-stable"
-
-apt update -y
-
-apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 docker version
 docker info
