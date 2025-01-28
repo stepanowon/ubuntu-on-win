d4f01026775e50c31aeb09b7f96d3355d1657cf9
stepanowon <stepanowon@hotmail.com>
2025-01-28 21:13:49 +0900

    apt upgrade 시에 prompt 멈춤현상 해결

--- scripts/install-essential-tools.sh
+++ scripts/install-essential-tools.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list.d/ubuntu.sources
 sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list.d/ubuntu.sources
 
-apt update -y
-apt upgrade -y
+apt update 
+#apt upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 apt install virtualbox-guest-additions-iso -y
 
